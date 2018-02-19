59f7602385eea01375f4d24acc63a326e548d98a
Matthew Moody <kratoes123@live.com>
2018-02-19 02:04:43 -0500

    Added in basic character movement

---
+++ Glitch/Assets/GlitchMade/Scripts/GlitchPlayerControl.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+
+	
+public class GlitchPlayerControl : MonoBehaviour
+{
+    Rigidbody rb;
+    protected Animator animator;
+
+    bool isHMoving;
+    bool isVMoving;
+    bool isJumping;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
+        isHMoving = false;
+        isVMoving = false;
+        isJumping = false;
+    }
+
+
+    void Update()
+    {
+        //get buttons down
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            isHMoving = true; 
+            animator.SetBool("Move", true);
+        }
+        if (Input.GetButtonDown("Vertical"))
+        {
+            isVMoving = true;
+            animator.SetBool("Move", true);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            animator.SetTrigger("Jump");
+           // rb.AddForce(transform.up +* 1300 .0f);
+            transform.Translate(0, Input.GetAxis("Vertical")  + 2.0f,0.0f);
+            Debug.Log("got it");
+        }
+
+        //get buttons up
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            isHMoving = false;
+        }
+        if (Input.GetButtonUp("Vertical"))
+        {
+            isVMoving = false;
+        }
+
+        if (isHMoving == false && isVMoving == false)
+        {
+            animator.SetBool("Move", false);
+        }
+
+        // check input variables
+
+        //move left or right
+        if (isHMoving == true)
+        {
+            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f,0,0);
+            Debug.Log("yee");
+        }
+        //move forward or backward
+        if (isVMoving == true)
+        {
+            transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * 10.0f);
+            Debug.Log("noice");
+        }
+
+
+    }
+
+}
---
+++ Glitch/Assets/GlitchMade/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+
+	
+public class PlayerMovementScript : MonoBehaviour
+{
+    Rigidbody rb;
+    protected Animator animator;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            transform.Translate(0, 0, Input.GetAxis("Horizontal") + 0.1f);
+            Debug.Log("yee");
+        }
+        else
+        {
+        }
+    }
+
+}
