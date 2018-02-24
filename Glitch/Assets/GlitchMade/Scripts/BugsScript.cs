c4132371be4ab942cc276469475da7b47ae9c679
Matthew Moody <kratoes123@live.com>
2018-02-23 22:42:31 -0500

    Added camera movement and script for bug button added bug button prefab

---
+++ Glitch/Assets/GlitchMade/Scripts/BugsScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugsScript : MonoBehaviour {
+    public GameObject player;
+
+    private bool isFall;
+	// Use this for initialization
+	void Start () {
+        isFall = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player.transform.position.y < -20.0f)
+        {
+            isFall = true;
+        }
+	}
+
+    void LateUpdate()
+    {
+        if(isFall == true)
+        {
+           // in
+        }
+    }
+}
---
+++ Glitch/Assets/GlitchMade/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public class CameraMovement : MonoBehaviour {
+    public GameObject player;
+    float x;
+    float y;
+    float z;
+    // Use this for initialization
+    void Start () {
+        x = player.transform.position.x;
+        y = player.transform.position.y + .1f;
+        z = player.transform.position.z + .1f;
+        transform.Translate(x, y, z);
+
+    }
+
+    void LateUpdate()
+    {
+        x = player.transform.position.x;
+        y = player.transform.position.y + 4.0f;
+        z = player.transform.position.z - 3.0f;
+        Vector3 newPos;
+        newPos.x = x;
+        newPos.y = y;
+        newPos.z = z;
+        transform.position = newPos;
+    }
+}
